cea4aa813f9467bc17b2c895a2ea774af9e095aa
Meliodas-999 <jameleddine2222@gmail.com>
2026-02-08 13:15:35 +0100

    implement responsive menu and cart icon components

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
-import Image from 'next/image'                                                  
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import CartIcon from "./CartIcon";
+
+const links = [
+  { id: 1, title: "Homepage", url: "/" },
+  { id: 2, title: "Menu", url: "/menu" },
+  { id: 3, title: "Working Hours", url: "/working-hours" },
+  { id: 4, title: "Contact", url: "/contact" },
+];
+
 const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const user = false;
   return (
     <div>
-      <Image src="/open.png" alt="Open Menu" width={24} height={24} />
-    </div>
-  )
-}
+      {!isOpen ? (
+        <Image
+          src="/open.png"
+          alt="Open Menu"
+          width={24}
+          height={24}
+          onClick={() => setIsOpen(true)}
+        />
+      ) : (
+        <Image
+          src="/close.png"
+          alt="Close Menu"
+          width={24}
+          height={24}
+          onClick={() => setIsOpen(false)}
+        />
+      )}
 
-export default Menu
+      {isOpen && (
+        <div className="bg-red-500 text-white absolute left-0 w-full top-24 h-[calc(100vh-6rem)] flex flex-col items-center justify-center text-3xl gap-8 z-10">
+        {links.map((item) => (
+          <Link href={item.url} key={item.id} onClick={() => setIsOpen(false)}>
+            {item.title}
+          </Link>
+        ))}
+        {!user ? (
+          <Link href="/login" onClick={() => setIsOpen(false)}>Login</Link>
+        ) : (
+          <Link href="/orders" onClick={() => setIsOpen(false)}>Orders</Link>
+        )}
+        <Link href="/cart" onClick={() => setIsOpen(false)}><CartIcon /></Link>
+      </div>
+      )}
+    </div>
+  );
+};
+
+export default Menu;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,33 @@
 import Link from 'next/link'
 import React from 'react'
 import Menu from './Menu'
+import Image from 'next/image'
+import CartIcon from './CartIcon'
 
 const NavBar = () => {
+  const user = false
   return (
     <div className='h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-red-500 uppercase'>
-      <div className='text-xl'>
+      <div className='hidden md:flex gap-4 flex-1'>
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+      <div className='text-xl md:font-bold flex-1 md:text-center'>
         <Link href="/">Massimo</Link>
       </div>
-      <div>
+      <div className='md:hidden'>
         <Menu />
+      </div>
+      <div className='hidden md:flex gap-4 items-center flex-1'>
+        <div className='flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+          <Image src="/phone.png" alt="phone" width={32} height={32} />
+          <span className='ml-2'>+1 234 567 890</span>
+        </div>
+        {!user ? <Link href="/login">Login</Link>
+        : ( <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon />
       </div>
     </div>
   )
