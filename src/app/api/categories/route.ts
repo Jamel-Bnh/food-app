19d5d38f9a4507f0117e2209cdb0d7422c9583dc
Meliodas-999 <jameleddine2222@gmail.com>
2026-02-18 01:04:36 +0100

    feat: integrate Prisma client and fetch categories from API
    - Added @prisma/client dependency to package.json
    - Updated GET route in categories API to fetch categories from the database
    - Modified MenuPage component to fetch categories from the new API endpoint
    - Created types for Menu, Product, Order, CartItem, and Cart in a new types.ts file

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,7 +1,18 @@
+import { PrismaClient } from "@prisma/client/extension";
 import { NextResponse } from "next/server"
 
- export const GET = () => {
-    return new NextResponse("Hello MF",{status:200});
+const prisma = new PrismaClient()
+
+ export const GET = async () => {
+   try{
+      const categories = await prisma.category.findMany()
+      return new NextResponse(JSON.stringify(categories),{status:500});
+   }
+   catch(err){
+      console.log(err)
+      return new NextResponse(JSON.stringify({message:"something went wrong"}),{status:500});
+   }
+    
  }
 
  export const PQST = () => {
