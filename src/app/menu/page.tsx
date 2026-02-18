a0c7a01f3456323291564367728b9e982729fa6c
Meliodas-999 <jameleddine2222@gmail.com>
2026-02-18 01:06:14 +0100

    fix: add type annotation for menu data in MenuPage component

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,4 +1,4 @@
-
+import { MenuType } from "@/types/types";
 import Link from "next/link";
 import React from "react";
 
@@ -11,7 +11,7 @@ const getData = async () => {
 }
 
 const MenuPage = async () => {
-  const menu = await getData();
+  const menu:MenuType = await getData();
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
